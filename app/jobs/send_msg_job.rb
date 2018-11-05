ea0d847719a903a52096a1016035b105e1a856f7
Stas Sychenkov <stasmoon@mail.ru>
2018-11-05 17:12:20 +0300

    Add rescue

--- app/jobs/send_msg_job.rb
+++ app/jobs/send_msg_job.rb
@@ -4,6 +4,9 @@ class SendMsgJob < ApplicationJob
     message = Message.find(id)
     MessengerService.send_message(message.provider, message.data)
     message.to_send!
+  rescue => error
+    message.make_system_error!
+    raise error
   end
 
 end
