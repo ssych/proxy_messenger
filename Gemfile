c5be7a641fabf7f871c3c7d31e87b2f44315f591
Stas Sychenkov <stasmoon@mail.ru>
2018-11-05 17:33:07 +0300

    Add auth

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'jbuilder'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'dotenv-rails'
 end
 
 group :development do
--- app/controllers/v1/application_controller.rb
+++ app/controllers/v1/application_controller.rb
@@ -1,3 +1,20 @@
 class V1::ApplicationController < ApplicationController
+  include ActionController::HttpAuthentication::Token::ControllerMethods
 
+  before_action :authenticate
+  
+  private
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      token == ENV['AUTH_TOKEN']
+    end
+  end
+
+  def render_unauthorized
+    head :unauthorized
+  end
 end
